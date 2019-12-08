335d683fcc21a75f2b0291ef39997a763b6b9114
Irina Lysenko <irina.lysenko21@gmail.com>
2019-12-08 15:26:14 +0200

    Updates

---
+++ precache-manifest.683a6296e6aaaf427ec8cf0fc6b1fe80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6db67dcca113772cdcb42407cb2d749",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "13828b75c927d3d76148",
+    "url": "/goit-react-hw-01-components/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "38dd1b1df61892cc6578",
+    "url": "/goit-react-hw-01-components/static/css/main.2cb6c25c.chunk.css"
+  },
+  {
+    "revision": "13828b75c927d3d76148",
+    "url": "/goit-react-hw-01-components/static/js/2.fc98bd29.chunk.js"
+  },
+  {
+    "revision": "38dd1b1df61892cc6578",
+    "url": "/goit-react-hw-01-components/static/js/main.6317d2fe.chunk.js"
+  },
+  {
+    "revision": "c4036953cb714bd39313",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.02e29828.js"
+  }
+]);
